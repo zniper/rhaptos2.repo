27d799b9860008ea494c2be2dc2e24df10e19295
Paul Brian <paul@mikadosoftware.com>
2012-04-16 14:28:23 +0100

    tinymce integration working OK

--- e2client/mikadosoftware-cnx.js
+++ e2client/mikadosoftware-cnx.js
@@ -13,14 +13,23 @@
         $("#logarea").html(txt + "<li> " + msg);
     };
 
+    function get_textarea_html5(){
+        //hardcoded textare ref here....
+        var txtarea = $('#e2textarea').tinymce().getContent();
+        return txtarea;
+    };
 
+    function display_textarea(){
+        txtarea = get_textarea_html5();
+        logout(txtarea);
+    };
 
     function sendajax(){
 	 //constants
          var TGTURL="http://hadrian/e2server/module/";
 
          var requestmethod = $('input:radio[name=method]:checked').val();
-         var payload = {'editortxt':$('#editorarea').val()}; 
+         var payload = {'editortxt':  get_textarea_html5()}; 
 
 	 var menuId = 42;
 	 var request = $.ajax({
@@ -49,7 +58,11 @@
 $(document).ready(function() {
 
     var TGTURL="http://hadrian/e2server/module/";
-    
+
+    //bind various clicks
+    $("#clickShowTextArea").click(function(e){display_textarea(); 
+                                              e.preventDefault()});
+
     logout('AJAX will fire at ' + TGTURL);    
     $("#click1").click(function(event){
                          sendajax();
