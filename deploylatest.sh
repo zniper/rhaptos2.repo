dcf86cb50d32c4ecee1719af34da344732fc74cf
Paul Brian <paul@mikadosoftware.com>
2012-05-07 18:33:54 +0100

    altered package hierachy, altered docs hierarchy, added logging and deployment of logging, improved config preprocessing. - part 3

--- deploylatest.sh
+++
@@ -1,6 +0,0 @@
-#make host=cnx1	fabfile=deploy/fab_app_frozone.py branch=screamingmess	context=office stageonly
-cd _config
-python mother_of_all_conf.py
-cd ..
-make host=cnx1	fabfile=deploy/fab_app_frozone.py branch=convertToOneRepoNoServer context=office oneinstall
-
