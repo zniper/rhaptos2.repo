4e92815537a763b2bf406c1aca53ff53a5a9145c
Paul Brian <paul@mikadosoftware.com>
2012-08-09 15:50:17 +0100

    use internal userID not openID as idnetifier in ACLs
    We want to migrate to a system of intetnal userids that map to many openids,
    so logins can go across the board.  I think this is true, but possibility it
    will mean loss of federation.

--- rhaptos2/repo/model.py
+++ rhaptos2/repo/model.py
@@ -92,20 +92,42 @@ ToDO:
   http://docs.python.org/library/functools.html#functools.wraps
 
 '''
+import memcache
 
 
+class User(object):
+    """
+    Is the user from memcache
+
+    Not sure there is a clear diff between Identity and User
+    """
+
+    def __init__(self, jsondoc):
+        """initialise from json doc """
+        self.userID = "org.cnx.user.f9647df6-cc6e-4885-9b53-254aa55a3383"
+  
 class Identity(object):
-    def __init__(self, identity_url):
+    def __init__(self, openid_url):
         """placeholder - we want to store identiy values somewhere
            but sqlite is limited to one server, so need move to network aware storage
         """
-        self.identity_url = identity_url
-        if self.identity_url:
-            self.email = 'Not Implemented' #pull from memcache
-            self.name = 'Not Implemented'
+        self.openid_url = openid_url
+        self.user = get_user_from_openid(openid_url)
+        if self.openid_url:
+            self.email = 'your email' 
+            self.name = 'your name'
+            self.userID = self.user.userID
         else:
             self.email = None
             self.name = None
+            self.userID = None
+
+def get_user_from_openid(openid_url):
+    """
+    """
+    #supposed to be memcache lookup
+    return User('')
+
 
 def store_identity(identity_url, **kwds):
     """no-op but would push idneity to backend storage ie memcvache """
@@ -118,6 +140,8 @@ def retrieve_identity(identity_url, **kwds):
 def whoami():
     '''
     return the identity url stored in session cookie
+    TODO: store the userid in session cookie too ?
+
     '''
     callstatsd("rhaptos2.repo.whoami")    
     app.logger.info('+++++ session dict' + repr(session.__dict__) )
@@ -127,7 +151,7 @@ def whoami():
         user = Identity(session['openid'])
         return user
     else:
-        app.logger.info('+++++ No session' + repr(g) )
+        callstatsd("rhaptos2.repo.notloggedin")    
         return None
         #is this alwasys desrireed?
 
