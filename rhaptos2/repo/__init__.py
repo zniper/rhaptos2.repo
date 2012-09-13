c673277fa5c20229e75a82d7aa6e40a461ddf880
Paul Brian <paul@mikadosoftware.com>
2012-09-13 14:32:18 +0100

    Added Log Handler for Statsd

--- rhaptos2/repo/__init__.py
+++ rhaptos2/repo/__init__.py
@@ -1,4 +1,4 @@
-#!/usr/local/bin/python
+#!/usr/bin/env python
 #! -*- coding: utf-8 -*-
 
 
@@ -14,15 +14,46 @@ import flask
 import statsd
 import json
 from functools import wraps
+import logging
 
-from rhaptos2.common import conf
-from rhaptos2.common import log
-from rhaptos2.common import err
+from rhaptos2.common import conf, log, err
 
 
 def set_logger(apptype, app_configd):
-    lg = log.get_logger(apptype, app_configd)
-    app.logger.addHandler(lg)
+    """
+lg.warn("Help", extra={'statsd':['rhaptos2.repo.module', 
+                                   'bamboo.foo.bar']})
+
+
+    """
+    lg = logging.getLogger(apptype)
+
+    #Trapping basic missing conf
+    uselogging = "%s_use_logging" % apptype
+    loglevel = "%s_loglevel" % apptype
+
+    if uselogging not in confd.keys():
+        confd[uselogging] = 'Y'
+
+    if loglevel not in confd.keys():
+        confd[loglevel] = 'DEBUG'
+
+    
+    hdlr = log.StatsdHandler(app.config['rhaptos2_statsd_host'],
+                    int(app.config['rhaptos2_statsd_port']))
+
+    hdlr2 = logging.StreamHandler()
+
+    formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s  - %(message)s')
+    hdlr.setFormatter(formatter)
+    hdlr2.setFormatter(formatter)
+
+    app.logger.addHandler(hdlr)
+    app.logger.addHandler(hdlr2)
+
+    app.logger.setLevel(confd[loglevel])
+
+
 
 
 def get_version():
