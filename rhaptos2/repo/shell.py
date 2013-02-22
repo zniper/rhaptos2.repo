db7f6f6e59dc996f68f88b9cad5b65f2bd8c21db
Paul Brian (lifeisstillgood) <paul@mikadosoftware.com>
2013-02-22 15:19:14 +0000

    updated shell command
    we know have db_session ready on the cmd line

--- rhaptos2/repo/shell.py
+++ rhaptos2/repo/shell.py
@@ -1,13 +1,41 @@
 #!/usr/bin/env python
+#! -*- coding: utf-8 -*-
 
-import warnings
-warnings.warn("This script to be rewritten in app facotry style")
+###
+# Copyright (c) Rice University 2012
+# This software is subject to
+# the provisions of the GNU Lesser General
+# Public License Version 2.1 (LGPL).
+# See LICENCE.txt for details.
+###
 
+"""
+dbtest=> drop table CNXfolder cascade;
+NOTICE:  drop cascades to constraint userrole_folder_folder_uuid_fkey on table userrole_folder
+DROP TABLE
+dbtest=> drop table userrole_folder cascade;
+DROP TABLE
+dbtest=>
+
+
+"""
+
+
+import decl
 import os
-import readline
-from pprint import pprint
+import pprint
+from rhaptos2.repo import backend, foldermodel
+from rhaptos2.repo.backend import db_session
+from rhaptos2.common import conf
 
-from flask import *
-from rhaptos2.repo import *
+HERE = os.path.abspath(os.path.dirname(__file__))
+CONFD_PATH = os.path.join(HERE, "../../local.ini")
+confd = conf.get_config(CONFD_PATH)
+
+pprint.pprint(confd['app'])
+backend.initdb(confd['app'])
+
+print "Running simple prepopulation of database as connected by:"
+print confd['app']
 
 os.environ['PYTHONINSPECT'] = 'True'
