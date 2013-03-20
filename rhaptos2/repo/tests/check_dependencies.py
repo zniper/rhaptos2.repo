2015509107784c3142a6b9a070b19712d4982114
Paul Brian (lifeisstillgood) <paul@mikadosoftware.com>
2013-03-20 13:08:35 +0000

    Useful tool to check dependencies of repo.
    It is nascent at best and needs expanding

--- rhaptos2/repo/tests/check_dependencies.py
+++ rhaptos2/repo/tests/check_dependencies.py
@@ -9,6 +9,26 @@
 # See LICENCE.txt for details.
 ###
 
+"""
+.. todo::
+   Are we running in venv?
+   Does that venv hold all needed requirements.
+
+
+CARBON_SERVER = 'log.frozone.mikadosoftware.com'
+CARBON_PORT = 2003
+STATSD_PORT = 8125
+STATSD_HOST = CARBON_SERVER
+
+import statsd 
+
+def test_statsd():
+
+    c = statsd.StatsClient(STATSD_HOST, STATSD_PORT)
+    for i in range(1000):
+        c.incr('rhaptos2.statsd.verify') 
+
+"""
 
 import sys
 
