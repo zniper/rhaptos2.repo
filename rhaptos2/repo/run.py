3fef4787598425534da8ec0a877beadd3be70860
Paul Brian (lifeisstillgood) <paul@mikadosoftware.com>
2013-04-11 18:19:40 +0100

    Fix license header
    the merging ended up with two license headers

--- rhaptos2/repo/run.py
+++ rhaptos2/repo/run.py
@@ -10,22 +10,17 @@
 ###
 
 
-"""run.py - Launch the repo app.
-
-Author: Paul Brian
-(C) 2012 Rice University
-
-This software is subject to the provisions of the GNU Lesser General
-Public License Version 2.1 (LGPL).  See LICENSE.txt for details.
-
-
-run.py
-------
+"""
+run.py - Launch the repo app.
 
 This is the suggested method for running a WSGI Server - we instantiate the repo
 app, and pass it to the waitress server (To be replaced by gunicorn)::
 
   python run.py --config=../../testing.ini
+
+.. todo::
+   Michaels solution to include a sererate standalone module was much better.
+   Replicate it and pull much of the URLMap code out of here.
 
 """
 
