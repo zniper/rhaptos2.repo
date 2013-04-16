b8b13308e9142b8302330c6511729a2dd3ba2995
Paul Brian (lifeisstillgood) <paul@mikadosoftware.com>
2013-04-16 23:12:52 +0100

    dev: start point

---
+++ rhaptos2/repo/uploader.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+#! -*- coding: utf-8 -*-
+
+### Copyright Rice University 2013
+
+# This program is licensed, without  under the terms of the
+# GNU Aferro Public License version 3.  Please see
+# LICENSE.txt for details
+
+###
+
+"""
+:author:  paul@mikadosoftware.com <Paul Brian>
+
+Stand alone uploader
+We dont want to mess the reactions of main repo
+with this, plus it seems likely we can simply add more and more ports
+
+"""
+
+
